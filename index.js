85158de63395654bdfb193f17173192f3f840d79
KevinLmn <ikebilol@gmail.com>
2022-09-27 11:12:06 +0200

    commit de l'exercice cowsay

---
+++ index.js
@@ -0,0 +1,7 @@
+const userInfo = require('./information');
+var cowsay = require("cowsay");
+console.log(cowsay.say({
+    text: `Hi, my name is ${userInfo.name} and I'm from ${userInfo.campus}`,
+    e: "oO",
+    T: "U "
+}));
---
+++ information.js
@@ -0,0 +1,7 @@
+const name = 'Kevin Lemniai';
+const campus = 'Paris';
+
+module.exports = {
+    name: name,
+    campus: campus,
+}
